f15c33bda77cf0b6cd8d2f715aa505c9b5092eb4
Yoann Lenoel <ylenoel@paul-f3Ar4s1.clusters.42paris.fr>
2023-09-24 17:08:45 +0200

    added tens/teens

--- src/convert.c
+++ src/convert.c
@@ -6,7 +6,7 @@
 /*   By: ylenoel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 15:03:01 by ylenoel           #+#    #+#             */
-/*   Updated: 2023/09/24 16:20:09 by ylenoel          ###   ########.fr       */
+/*   Updated: 2023/09/24 17:05:41 by ylenoel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,24 @@ void	convert(char *dict, char *nb)
 	i = 0;
 	while (nb[i] != 0)
 	{
-		if(ft_strlen(&nb[i]) % 3 == 0)
+		if (ft_strlen(&nb[i]) % 3 == 0)
 		{
 			ft_putstr_until_whitespace(search_ones(dict, nb[i]));
 			ft_putstr_until_whitespace(search_big(dict, 3));
+			i++;
 		}
-		if(nb[i] <= 9)
+		if (nb[i] == '1')
+		{
+			ft_putstr_until_whitespace(search_teens(dict, nb[i], nb[i + 1]));
+				i++;
+		}
+		else if (nb[i] > '1')
+		{
+			ft_putstr_until_whitespace(search_tens(dict, nb[i]));
+			i++;
+			ft_putstr_until_whitespace(search_ones(dict, nb[i]));
+		}
+		if (nb[i] <= 9)
 		{
 			ft_putstr_until_whitespace(search_ones(dict, nb[i]));
 		}
